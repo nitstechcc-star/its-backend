9be76f22ca228fbb08e0022599f6768fb724ed1f
mn <nitstechcc@gmail.com>
2026-05-18 02:53:28 +0200

    superuser update

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations app
 python manage.py migrate
 
-if [[$CREATE_SUPERUSER == "True"]];
+if [[$CREATE_SUPERUSER == True]];
 then
     python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
 fi
