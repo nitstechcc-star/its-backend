b0ff9efa0fb48b882964ddfa0517977f04d72902
mn <nitstechcc@gmail.com>
2026-05-18 02:30:33 +0200

    superuser update

--- build.sh
+++ build.sh
@@ -8,4 +8,7 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations app
 python manage.py migrate
 
-
+if [[$CREATE_SUPERUSER]];
+then
+    python manage.py createsuperuser --noinput
+fi
